191e932db6a1f2c63995d514cc775412a6f90207
Gyulari <Gyulari7160@gmail.com>
2022-12-01 09:14:55 +0900

    몬스터 AI onSight 확인 함수 추가 및 임시 Collision 검사 추가 (방어 기능 추가 목적, 제작 중)

--- Assets/02. Script/MonsterController.cs
+++ Assets/02. Script/MonsterController.cs
@@ -5,6 +5,47 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     [SerializeField]
+    private float AISight = 2.5f;
+    [SerializeField]
+    private float traceSpeed = 0.05f;
+
+    public GameObject player;
+
+    private bool sequence = true;
+    private bool taskAction = false;
+
+    private void FixedUpdate()
+    {
+        taskAction = onSight(player);
+
+        if (!taskAction) {
+            Vector3 traceDirection = (player.transform.position - gameObject.transform.position).normalized;
+            gameObject.transform.position += traceDirection * traceSpeed;
+        }
+
+        else
+            Debug.Log("Attcak!");
+    }
+
+    bool onSight(GameObject player)
+    {
+        float toPlayerDistance = Vector3.Distance(player.transform.position, gameObject.transform.position);
+
+        if (toPlayerDistance <= AISight)
+            return true;
+
+        else
+            return false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Debug.Log("Hit");
+    }
+
+    /*
+    [SerializeField]
     private float traceSpeed = 0.05f;
     [SerializeField]
     private float delayTime = 3.0f;
@@ -40,4 +81,5 @@ public class MonsterController : MonoBehaviour
             onAttack = false;
         }
     }
+    */
 }
